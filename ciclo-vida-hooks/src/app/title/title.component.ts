589364ed729644fd973acadf6a0edf29d3dd7e40
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-06-02 17:53:49 -0300

    OnInit
    OnInit é um evento quando nosso componente é iniciado.
    
    Podemos usar ele  para passar parametros sempre que iniciamos um component.
    
    Dessa forma sempre que nasce o component podemos usar para colocar ou importar algo como nome, um menu de opções entre outras coisas.

--- ciclo-vida-hooks/src/app/title/title.component.ts
+++ ciclo-vida-hooks/src/app/title/title.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit{
+  nome: string = "Henrique";
 
+  constructor() {}
+  ngOnInit(): void {
+    this.nome = `Ola ${this.nome}`;
+    console.log("OnInit iniciado")
+  }
 }
